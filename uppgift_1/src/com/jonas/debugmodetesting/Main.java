c37ace96586fa2949c462ec190d3cb13b7f4a18b
Jonas Ullvahn <ullvahn@gmail.com>
2023-10-02 00:17:58 +0200

    Add for-loop  and breakpoints

--- uppgift_1/src/com/jonas/debugmodetesting/Main.java
+++ uppgift_1/src/com/jonas/debugmodetesting/Main.java
@@ -1,4 +1,17 @@
 package com.jonas.debugmodetesting;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        int[] number = {5,10,50,100};
+
+        for (int i = 0; i < number.length; i++) {
+            if(number[i] == number[2]){
+                continue;
+            }
+            System.out.println(number[i]);
+
+        }
+    }
 }
